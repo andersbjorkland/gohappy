20ce487f719ed5f84d533f6bfda6b15c39ab849d
Anders Björkland <git@andersbjorkland.online>
2020-11-03 13:47:43 +0100

    Initial Commit.

---
+++ assets/js/scripts.js
@@ -0,0 +1,15 @@
+import "../css/styles.css"
+
+// Our modules / classes
+// import MobileMenu from "./modules/MobileMenu"
+
+
+
+// Instantiate a new object using our modules/classes
+// let mobileMenu = new MobileMenu()
+
+
+// Allow new JS and CSS to load in browser without a traditional page refresh
+if (module.hot) {
+  module.hot.accept()
+}
---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+get_header();
+?>
+<p>Initial setup</p>
+<?php get_footer(); ?>
