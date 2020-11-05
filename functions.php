24ac3cf6794d4f9da7488defd33c8c9f68f920a5
Anders Björkland <git@andersbjorkland.online>
2020-11-05 19:52:52 +0100

    Updated styles.

--- functions.php
+++ functions.php
@@ -1,15 +1,15 @@
 <?php
 
 function theme_files() {
-	wp_enqueue_style( 'custom-google-fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i' );
+	wp_enqueue_style( 'custom-google-fonts', 'https://fonts.googleapis.com/css?family=Archivo+Black|Roboto:100,300,300i,400,500;700' );
 	wp_enqueue_style( 'font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css' );
 
 	if ( strstr( $_SERVER['SERVER_NAME'], 'andersbjorkland.local' ) ) {
 		wp_enqueue_script( 'main-university-js', 'http://localhost:3000/bundled.js', null, '1.0', true );
 	} else {
 		wp_enqueue_script( 'our-vendors.js', get_theme_file_uri( '/bundled-assets/undefined' ), null, '1.0', true );
-		wp_enqueue_script( 'main-scripts-js', get_theme_file_uri( '/bundled-assets/scripts.9f8517c275ca3e15938c.js' ), null, '1.0', true );
-		wp_enqueue_style( 'our-main-styles', get_theme_file_uri( '/bundled-assets/styles.9f8517c275ca3e15938c.css' ) );
+		wp_enqueue_script( 'main-scripts-js', get_theme_file_uri( '/bundled-assets/scripts.5e6959f5df3dde2b04b4.js' ), null, '1.0', true );
+		wp_enqueue_style( 'our-main-styles', get_theme_file_uri( '/bundled-assets/styles.5e6959f5df3dde2b04b4.css' ) );
 	}
 }
 
