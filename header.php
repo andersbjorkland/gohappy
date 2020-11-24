7d37cd97cd77cdce9d65e324700aeace8fe8fee1
Anders Björkland <git@andersbjorkland.online>
2020-11-24 13:22:20 +0100

    Added a content-wrap class. Added style to about page.

--- header.php
+++ header.php
@@ -20,9 +20,6 @@
             <a class="nav__item <?php if (is_home()) echo 'active'; ?>" href="<?php echo site_url(); ?>">
                 <div class="aria-link">Home</div>
             </a>
-            <a class="nav__item" href="#">
-                <div class="aria-link">Portfolio</div>
-            </a>
             <a class="nav__item <?php if (get_post_type() === 'post' && !is_home()) echo 'active'; ?>" href="#">
                 <div class="aria-link">Blog</div>
             </a>
--- page-about.php
+++ page-about.php
@@ -4,10 +4,14 @@ get_header();
 while (have_posts()) {
     the_post();
 ?>
-    <h1 class="aria-hidden">About</h1>
-    <div class="content">
-        <?php the_content(); ?>
+<article class="content-wrap side-padding">
+    <div class="content-container mb4">
+        <h1 class="aria-hidden">About</h1>
+        <div class="content">
+            <?php the_content(); ?>
+        </div>
     </div>
+</article>
 <?php
 }
 get_footer();
